f43b03331219f3a38f316e6c85c765a41b420b18
Taukurade <taukurade@gmail.com>
2022-04-10 15:00:59 +0500

    made with love :3

--- cogs/keyboards.py
+++ cogs/keyboards.py
@@ -1,4 +1,5 @@
-from vkbottle import Keyboard, KeyboardButtonColor, Text, Location, OpenLink
+import re
+from vkbottle import Keyboard, KeyboardButtonColor, Text, Location, OpenLink, ShowSnackbarEvent
 def start_keyb():
     key = Keyboard(one_time=True)
     key.add(Text('Опрос'), KeyboardButtonColor.PRIMARY)
@@ -29,4 +30,13 @@ def questions():
     #где находится Питер?
     d[4].add(Location({5}))
     #вы поставите мне звездочку на гитхабе?
-    d[5].add(OpenLink(''))
+    d[5].add(OpenLink('https://github.com/Taukurade/omnicode-vk-bot','Поставить',{6}))
+    #здеь несколько кнопок?
+    d[6].add(Text('Да',{7}), KeyboardButtonColor.SECONDARY)
+    d[6].add(Text('Да',{7}), KeyboardButtonColor.SECONDARY)
+    d[6].add(Text('Да',{7}), KeyboardButtonColor.SECONDARY)
+    #здесь несколько разных кнопок?
+    d[7].add(Text('Да',{8}), KeyboardButtonColor.PRIMARY)
+    d[7].add(Text('Ага',{8}), KeyboardButtonColor.SECONDARY)
+    d[7].add(Text('Нет(Да)',{8}), KeyboardButtonColor.NEGATIVE)
+    return d
